352c21c81b0eacd3df47f138b8410f8da4c7e21d
hill1895 <lx830075@163.com>
2014-06-08 19:47:35 +0800

    some changes in front-end design
    using jQuery to replace javascript and add some animations

--- eating/main.js
+++ eating/main.js
@@ -1,14 +1,63 @@
 // JavaScript Document
 
 
-
-function showdiv(div)
-{	var vis=document.getElementById(div);
-	vis.style.display='block';
-	}
-
+$(document).ready(function(){
+	var h_page=$(document).height();
+	var w_page=$(document).width();
+	$('#mask').css({"height":h_page+'px',"width":w_page+'px','left':'0px','top':'0px'});
 	
-function hidediv(div)
-{	var vis=document.getElementById(div);
-	vis.style.display='none';
-	}
+
+	$('#addnewbutton').click(function()
+	{	var s_top=$(document).scrollTop();
+		$('#addnew').css('top',s_top+50+'px').fadeIn('slow').show();
+	$('#mask').fadeIn('slow').show();
+	
+		})
+	$("input[name='close_addnew']").click(function()
+	{
+		$('#addnew').fadeOut('slow');
+		$('#mask').fadeOut('slow');
+		})	
+	
+	$("input[name='cancel_addnew']").click(function()
+	{
+		$('#addnew').fadeOut('slow');
+		$('#mask').fadeOut('slow');
+		})	
+		
+		
+	$('#showdetails').click(function()
+	{	var s_top=$(document).scrollTop();
+		$('#details').css('top',s_top+50+'px').fadeIn('slow').show();
+		$('#mask').fadeIn('slow').show();
+		})
+		
+		$("#closedetails").click(function()
+	{
+		$('#details').fadeOut('slow');
+		$('#mask').fadeOut('slow');
+		})	
+		
+		$("#cancel_details").click(function()
+	{
+		$('#details').fadeOut('slow');
+		$('#mask').fadeOut('slow');
+		})	
+		
+		$('#wein').click(function()
+	{	var s_top=$(document).scrollTop();
+		$('#participate').css('top',s_top+100+'px').fadeIn('slow').show();
+		})
+		
+		$("#close_participate").click(function()
+	{
+		$('#participate').fadeOut('slow');
+
+		})	
+	
+	
+	})
+
+
+
+
--- main.js
+++ main.js
@@ -1,37 +1,75 @@
 // JavaScript Document
 
-var div0,div1;
-window.onload=function()
-{
-var w=document.body.scrollWidth; 
-var h=document.body.scrollHeight;
-div0=document.getElementById('all');
-div1=document.getElementById('mask');
-div0.style.width=w+"px";
-div0.style.height=h+"px";
-div1.style.width=w+"px";
-div1.style.height=h+"px";
-}
+
+///placeholder插件/
+(function ($) {
+    $.fn.extend({
+        placeholder : function () {
+            if ("placeholder" in document.createElement("input")) {
+                return this //如果原生支持placeholder属性，则返回对象本身
+            } else {
+
+                return this.each(function () {				
+                    var _this = $(this);
+                    _this.val(_this.attr("placeholder")).focus(function () {
+                        if (_this.val() === _this.attr("placeholder")) {
+                            _this.val("")
+                        }
+                    }).blur(function () {
+                        if (_this.val().length === 0) {
+                            _this.val(_this.attr("placeholder"))
+                        }
+                    })
+                })
+            }
+        }
+    })
+})(jQuery);
 
 
 
+//主体
+$(document).ready(function(){
+	var h_page=$(document).height();
+	var w_page=$(document).width();
 
-function showlogin()
-{	 var vis=document.getElementById('login_register');
-		vis.style.display='block';
-div1=document.getElementById('mask');
-	div1.style.display='block';
-
+	$('#mask').css({"height":h_page+'px',"width":w_page+'px','top':'0px','left':'0px'});
+	$('#reg_log').click(function()
+	{	
+		$('#login_register').fadeIn('slow').show();
+	$('#mask').fadeIn('slow').show();
+		})
+	$('#closebutton').click(function()
+	{
+		$('#login_register').fadeOut('slow');
+		$('#mask').fadeOut('slow');
 	
-	}
-	
-function hidelogin()
-{	 var vis=document.getElementById('login_register');
-		vis.style.display='none';
+		})	
 		
-div1=document.getElementById('mask');
-	div1.style.display="none";
-
+		
+			
+		$('#feature1').hover(
+		function(){
+		$('#hotmask').fadeIn(500);
+		},function(){$('#hotmask').fadeOut(500);	})
+		
+$('input[placeholder][type=text]').placeholder();
 	
-	}
+
+		
+			
+	
+		
+			
+			
+			
+	
+	
+	
+	})
+
+
+
+
+
 	
