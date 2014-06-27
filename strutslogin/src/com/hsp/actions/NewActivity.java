3acde9b0b4f8fe35bffa541b0f2f87879c3569b4
momo9 <liangchenchi@gmail.com>
2014-06-27 20:04:04 +0800

    Refactor Add-Activity
    Move AddActivityAction to model. Just provide a interface to controllor.

--- strutslogin/src/com/hsp/actions/NewActivity.java
+++ strutslogin/src/com/hsp/actions/NewActivity.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.hsp.model.Activity;
-import com.hsp.model.DatabaseAction;
+import com.hsp.model.AddActivityAction;
 
 /**
  * Servlet implementation class NewActivity
@@ -37,7 +37,7 @@ public class NewActivity extends HttpServlet {
 		// TODO Auto-generated method stub
 	  this.request = request;
 	  getAllPara();
-	  new AddActivityAction().doSql(act);
+	  new AddActivityAction().addActivity(act);
 	  response.sendRedirect(retUrl);
 	}
 	
@@ -90,50 +90,4 @@ public class NewActivity extends HttpServlet {
 	  return cal.getTime();
 	}
 	
-	private class AddActivityAction extends DatabaseAction {
-
-    @Override
-    protected Object sqlAction(Connection conn, Object para) throws SQLException {
-      // TODO Auto-generated method stub
-      Activity locact = (Activity) para;
-      
-      String str =
-        "insert into activities values(" +
-        "?," + // activityid
-        "?," + // userid
-        "?," + // activityname
-        "?," + // activityowner
-        "?," + // activitycontact
-        "?," + // activitytype
-        "?," + // activitypos
-        "?," + // acttimebeg
-        "?," + // acttimeend
-        "?," + // actmaxnumber
-        "?," + // acttakenumber
-        "?," + // activityrange
-        "?"  + // activitydetail 
-        ");";
-      statement = conn.prepareStatement(str);
-      
-      statement.setString(1, null);      // activityid
-      statement.setInt(2, 1);            // userid, cannot be acquired now, so use fix value 1.
-      statement.setString(3, locact.getActivityName());// activityname
-      statement.setString(4, locact.getOrganizer());// activityowner
-      statement.setString(5, locact.getContact());// activitycontact
-      statement.setString(6, locact.getActivityClass());// activitytype
-      statement.setString(7, locact.getActivityPlace());// activitypos
-      statement.setTimestamp(8, new Timestamp(locact.getStarttime().getTime()));// acttimebeg
-      statement.setTimestamp(9, new Timestamp(locact.getEndtime().getTime()));// acttimeend
-      statement.setInt(10, locact.getMaxNumber());// actmaxnumber
-      statement.setInt(11, 1);            // acttakenumber, at first, only the organizer
-      statement.setString(12, locact.getRange());// activityrange
-      statement.setString(13, locact.getDetail());// activitydetail 
-      
-      statement.executeUpdate();
-      
-      return null;
-    }
-	  
-	}
-
 }
